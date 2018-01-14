2629cddb4ee8a1eeeb36851c5ec25a1cc75b8194
vancisin peter <vancisin.peter@gmail.com>
2018-01-14 22:57:28 +0100

    Add project files.

---
+++ nakovChp22-01ExtdMtodSubstring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nakovChp22_01ExtdMtodSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
